60492b46c99236aeea423a866692332dc211cc5b
tommaso.panciroli <tommypanci@gmail.com>
2025-02-18 16:12:00 +0100

    setup

---
+++ src/tipo/Dipendente.java
@@ -0,0 +1,11 @@
+package tipo;
+
+public class Dipendente {
+    private Long matricola;
+    private int stipendio;
+    private enum Dipartimento {DIPARTIMENTO, PRODUZIONE, VENDITE}
+
+    public Dipendente (Long numMatricola, int stipendio, Dipartimento){}
+
+
+}
